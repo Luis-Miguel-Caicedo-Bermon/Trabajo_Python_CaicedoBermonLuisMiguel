4d6f49bfb13eb3d6bdb2eb23e01492e5ad2e54ce
Andres <martinezmiguel.0098@gmail.com>
2024-08-12 12:37:42 -0500

    docs :memo: decumentacion del código

--- main.py
+++ main.py
@@ -1,81 +1,81 @@
 import json
-from datetime import datetime,timezone
-def Abrircompras():
+from datetime import datetime,timezone #importación para generar la fecha
+def Abrircompras():#función para abrir el archivo json
     datos=[]
     with open("Compras.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarcompras(midato):
+def guardarcompras(midato):#función para guardar cambios en el json
     with open("Compras.json", "w") as mifile:
         json.dump(midato,mifile)
-def AbrirEmpleados():
+def AbrirEmpleados():#función para abrir el archivo json
     datos=[]
     with open("Empleados.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarEmpleados(midato):
+def guardarEmpleados(midato):#función para guardar cambios en el json
     with open("Empleados.json", "w") as mifile:
         json.dump(midato,mifile)
-def AbrirMedicamentos():
+def AbrirMedicamentos():#función para abrir el archivo json
     datos=[]
     with open("Medicamentos.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarMedicamentos(midato):
+def guardarMedicamentos(midato):#función para guardar cambios en el json
     with open("Medicamentos.json", "w") as mifile:
         json.dump(midato,mifile)
-def AbrirPacientes():
+def AbrirPacientes():#función para abrir el archivo json
     datos=[]
     with open("Pacientes.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarPacientes(midato):
+def guardarPacientes(midato):#función para guardar cambios en el json
     with open("Pacientes.json", "w") as mifile:
         json.dump(midato,mifile)
-def AbrirProveedores():
+def AbrirProveedores():#función para abrir el archivo json
     datos=[]
     with open("Proveedores.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarProveedores(midato):
+def guardarProveedores(midato):#función para guardar cambios en el json
     with open("Proveedores.json", "w") as mifile:
         json.dump(midato,mifile)
-def AbrirVentas():
+def AbrirVentas():#función para abrir el archivo json
     datos=[]
     with open("Ventas.json", encoding="utf8") as openfile:
         datos=json.load(openfile)
         return datos
 
-def guardarVentas(midato):
+def guardarVentas(midato):#función para guardar cambios en el json
     with open("Ventas.json", "w") as mifile:
         json.dump(midato,mifile)
 
-def menu():
+def menu():#menú de opciones
     print("------Menú------\n"
           "1. ventas\n"
           "2. compras\n"
           "3. salir\n")
 bucle=True
-while bucle==True:
+while bucle==True:#bucle para no cerrar el programa mientras se requiera
     menu()
     opc=int(input("Selecciona una opción: "))
     if opc==1:
         empleados=AbrirEmpleados()
-        medicamentos=AbrirMedicamentos()
+        medicamentos=AbrirMedicamentos()#se abren los archivos json
         ventas=AbrirVentas()
         pacientes=AbrirPacientes()
         print("----------VENTAS----------")
         print("informacion del paciente")
         nombre_paciente=input("ingresa el nombre: ")
-        direccion_paciente=input("ingrese la direccion del paciente: ")
+        direccion_paciente=input("ingrese la direccion del paciente: ")#se piden los datos
         numero_paciente=input("Ingresa el numero del paciente: ")
         print("Empleados")
-        for i in empleados:
+        for i in empleados:#muestra todos los empleados
             print("-----------------------------")
             print("Nombre: ", i["nombre"])
             print("-----------------------------")
@@ -84,10 +84,10 @@ while bucle==True:
         while bucle2==True:
             nombre_empleado=input("Escribe el nombre del empleado que realizo la venta\n")
             for i in empleados:
-                if i["nombre"]==nombre_empleado:
+                if i["nombre"]==nombre_empleado:#verifica que el empleado existe
                     cargo=i["cargo"]
                     bucle2=False
-        for i in medicamentos:
+        for i in medicamentos:#muestra medicamentos
             print("-----------------------------")
             print("Nombre: ", i["nombre"])
             print("-----------------------------")
@@ -96,7 +96,7 @@ while bucle==True:
         while bucle3==True:
             nombre_medicamento=input("Escribe el nombre del medicamento: ")
             for i in medicamentos:
-                if i["nombre"]==nombre_medicamento:
+                if i["nombre"]==nombre_medicamento:#verifica que el medicamento existe
                     bucle4=True
                     while bucle4==True:
                         cantidad=int(input("cuanto quieres de este producto: "))
@@ -105,25 +105,25 @@ while bucle==True:
                             precio=i["precio"]
                             bucle4=False
                     bucle3=False
-        fecha=datetime.now(timezone.utc)
-        fecha_organizada=fecha.strftime("%Y-%m-%dT%H:%M:%SZ")
+        fecha=datetime.now(timezone.utc)#obtiene la fecha
+        fecha_organizada=fecha.strftime("%Y-%m-%dT%H:%M:%SZ")#organiza la fecha con un nuevo formato
         ventas.append({"fechaVenta":fecha_organizada,"paciente":{"nombre":nombre_paciente,"direccion":direccion_paciente},
                        "empleado":{"nombre":nombre_empleado, "cargo":cargo},"medicamentosVendidos":{"nombreMedicamento":nombre_medicamento,"cantidadVendida":cantidad,"precio":precio}})
         pacientes.append({"nombre":nombre_paciente,"direccion":direccion_paciente,"telefono":numero_paciente})
         guardarMedicamentos(medicamentos)
-        guardarVentas(ventas)
+        guardarVentas(ventas)#guardan los cambios realizados
         guardarPacientes(pacientes)
     
     if opc==2:
         compras=Abrircompras()
-        medicamentos=AbrirMedicamentos()
+        medicamentos=AbrirMedicamentos()#se abren los archivos json
         proveedores=AbrirProveedores()
         print("--------COMPRAR--------")
         print("Información del proveedor")
         nombre_proveedor=input("Nombre: ")
-        contacto_proveedor=input("Contacto: ")
+        contacto_proveedor=input("Contacto: ")#se piden los datos
         direccion_proveedor=input("Dirección: ")
-        for i in medicamentos:
+        for i in medicamentos:#muestra medicamentos
             print("-----------------------------")
             print("Nombre: ", i["nombre"])
             print("-----------------------------")
@@ -131,18 +131,18 @@ while bucle==True:
         while bucle3==True:
             nombre_medicamento=input("Escribe el nombre del medicamento: ")
             for i in medicamentos:
-                if i["nombre"]==nombre_medicamento:
+                if i["nombre"]==nombre_medicamento:#verifica que el medicamento existe
                     cantidad=int(input("cuanto quieres de este producto: "))
                     i["stock"]+=cantidad
                     bucle3=False
         precio_compra=int(input("Precio: "))
-        fecha=datetime.now(timezone.utc)
-        fecha_organizada=fecha.strftime("%Y-%m-%dT%H:%M:%SZ")
+        fecha=datetime.now(timezone.utc)#obtiene la fecha
+        fecha_organizada=fecha.strftime("%Y-%m-%dT%H:%M:%SZ")#organiza la fecha con un nuevo formato
         compras.append({"fechaCompra":fecha_organizada,"proveedor":{"nombre":nombre_proveedor,"contacto":contacto_proveedor},
                         "medicamentosComprados":[{"nombreMedicamento":nombre_medicamento,"cantidadComprada":cantidad,"precioCompra":precio_compra}]})
         proveedores.append({"nombre":nombre_proveedor,"contacto":contacto_proveedor,"direccion":direccion_proveedor})
         guardarcompras(compras)
-        guardarProveedores(proveedores)
+        guardarProveedores(proveedores)#guardan los cambios realizados
         guardarMedicamentos(medicamentos)
     if opc==3:
-        bucle=False
+        bucle=False#finaliza el programa
